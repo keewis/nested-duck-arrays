67d01546e5e0b97d305fe741f14eb05411369830
Justus Magin <keewis@posteo.de>
2024-07-01 14:14:08 +0200

    expose the new functions

--- nested_duck_arrays/__init__.py
+++ nested_duck_arrays/__init__.py
@@ -1,5 +1,7 @@
 from importlib.metadata import version
 
+from nested_duck_arrays.api import first_layer, layers  # noqa: F401
+
 try:
     __version__ = version("nested_duck_arrays")
 except Exception:
